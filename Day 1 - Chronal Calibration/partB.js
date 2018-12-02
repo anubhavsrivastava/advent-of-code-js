f4045c75f0e5e413bd80832f017e3d64151ef930
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-12-02 10:28:51 +0530

    optimised partB of Problem1

--- Day 1 - Chronal Calibration/partB.js
+++ Day 1 - Chronal Calibration/partB.js
@@ -1,15 +1,15 @@
 const calibrationB = inputArray => {
 	let frequencySum = 0;
-	const foundFrequencies = [];
+	const foundFrequencies = {};
 	let found;
 
 	while (!found) {
 		for (const frequency of inputArray) {
 			frequencySum += +frequency;
-			if (foundFrequencies.includes(frequencySum)) {
+			if (foundFrequencies[frequencySum]) {
 				found = true;
-				return foundFrequencies[foundFrequencies.length - 1] + +frequency;
-			} else foundFrequencies.push(frequencySum);
+				return frequencySum;
+			} else foundFrequencies[frequencySum] = true;
 		}
 	}
 };
